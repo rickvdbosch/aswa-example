b1e0c493060f74b132035e567149d4a29601e45c
Rick van den Bosch <rickvdbosch@outlook.com>
2020-05-27 13:16:09 +0200

    Fixed function 🤓

--- aswa-api/GetValues/index.ts
+++ aswa-api/GetValues/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const responseMessage = '{ "Values": ["Hello", "world", "from", "TypeScript", "Functions"] }';
+    const responseMessage = '["Hello", "world", "from", "TypeScript", "Functions"]';
 
     context.res = {
         body: responseMessage
