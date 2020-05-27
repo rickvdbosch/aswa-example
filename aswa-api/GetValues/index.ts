2dcd29185647534f233bad5fa4988e6b25952185
Rick van den Bosch <rickvdbosch@outlook.com>
2020-05-27 12:06:01 +0200

    'Implemented' GetValues

---
+++ aswa-api/GetValues/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions'
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    const responseMessage = '{ "Values": ["Hello", "world", "from", "TypeScript", "Functions"] }';
+
+    context.res = {
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
