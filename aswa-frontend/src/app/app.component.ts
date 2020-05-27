4bdb0f9b17e7212a17497bc1156a65d24c50edcb
Rick van den Bosch <rickvdbosch@outlook.com>
2020-05-27 13:16:31 +0200

    Implemented calling an API form the frontend

--- aswa-frontend/src/app/app.component.ts
+++ aswa-frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+const baseUrl: string = environment.baseApiUrl;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aswa-frontend';
+export class AppComponent implements OnInit {
+
+  public title: string = 'Azure Static Web Apps';
+  public values$: Observable<Array<string>>;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.values$ = this.httpClient.get<Array<string>>(`${baseUrl}values`);
+  }
 }
--- aswa-frontend/src/app/app.module.ts
+++ aswa-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
