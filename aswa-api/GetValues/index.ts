c4d745f2ca904075ecee9177365d8a32745c8ad6
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-09 15:01:42 +0200

    Functions update

--- aswa-api/GetValues/index.ts
+++ aswa-api/GetValues/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.res = {
-        body: '["Hello", "world", "from", "TypeScript", "Functions"]'
+        body: '["Hello", "world", "from", "Betatalks", "TypeScript", "Functions"]'
     };
 
 };
